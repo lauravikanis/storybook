d0e259f7f7d700ed4f3bf95888e1070ac5354979
lauravikanis <lauravikanis@yahoo.de>
2020-10-05 16:27:56 +0200

    add button

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,8 +1,10 @@
 import "./button.css";
 import buttonText from "./button-text.html";
 import buttonLogo from "./button-logo.html";
+import buttonPlay from "./button-play.html";
 
 export default { title: "Components/Button" };
 
 export const withText = () => buttonText;
 export const withLogo = () => buttonLogo;
+export const play = () => buttonPlay;
