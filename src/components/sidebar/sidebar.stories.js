c76133850aea98612f81aa75b8b36a968635b7e0
lauravikanis <lauravikanis@yahoo.de>
2020-10-07 14:01:19 +0200

    add list

---
+++ src/components/sidebar/sidebar.stories.js
@@ -0,0 +1,8 @@
+import "./sidebar.css";
+import sidebarItem from "./sidebar-list-item.html";
+import sidebar from "./sidebar.html";
+
+export default { title: "Components/Sidebar" };
+
+export const navItem = () => sidebarItem;
+export const nav = () => sidebar;
