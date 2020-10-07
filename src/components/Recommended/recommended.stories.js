08bf72f8e6c3aff10132253a89366b95d5010806
lauravikanis <67901192+lauravikanis@users.noreply.github.com>
2020-10-07 13:30:29 +0200

    Revert "add recommend card"

--- src/components/Recommended/recommended.stories.js
+++
@@ -1,6 +0,0 @@
-import "./recommended.css";
-import cardRecommended from "./recommended.html";
-
-export default { title: "Components/Recommended" };
-
-export const recommended = () => cardRecommended;
