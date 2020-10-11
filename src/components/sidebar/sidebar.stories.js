2fb957f7de569a7e7464a371366b3dc697b8af22
lauravikanis <lauravikanis@yahoo.de>
2020-10-11 17:17:41 +0200

    delete redundant sideNav

--- src/components/sidebar/sidebar.stories.js
+++
@@ -1,8 +0,0 @@
-import "./sidebar.css";
-import sidebarItem from "./sidebar-list-item.html";
-import sidebar from "./sidebar.html";
-
-export default { title: "Components/Sidebar" };
-
-export const navItem = () => sidebarItem;
-export const nav = () => sidebar;
