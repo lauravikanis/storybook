79781bab6b7bdd60ddbfaa2d2fc3b35908c09afa
lauravikanis <lauravikanis@yahoo.de>
2020-10-13 16:54:38 +0200

    tuesday class

--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -5,7 +5,15 @@ export default {
   parameters: { layout: "centered" },
 };
 
-export const beatIt = () => createTrackElement("Beat It", "Michael Jackson");
+export const beatIt = () =>
+  createTrackElement({
+    title: "Beat It",
+    artist: "Michael Jackson",
+    imgSrc:
+      "https://i.pinimg.com/564x/2c/7d/99/2c7d99b45744abba70aea6be9127ea34.jpg",
+    audioSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Cambo/Chilled_Beats/Cambo_-_02_-_Warm.mp3",
+  });
 
 export const thriller = () => createTrackElement("Thriller", "Michael Jackson");
 
@@ -14,3 +22,6 @@ export const dontStop = () =>
 
 export const billieJean = () =>
   createTrackElement("Billie Jean", "Michael Jackson");
+
+// export const thriller = () =>
+//   createTrackElement({ title: "ABC", artist: "sfdsj", imgSrc: "fd" });
