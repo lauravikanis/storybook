6e10b7385f70b406712b908372732115a996cb3d
lauravikanis <lauravikanis@yahoo.de>
2020-10-17 22:22:48 +0200

    add images edit css

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -2,13 +2,18 @@ import "./player.css";
 import forwardElementSrc from "../../assets/forward-primary.svg";
 import playElementSrc from "../../assets/play-primary.svg";
 import backwardElementSrc from "../../assets/backward-primary.svg";
+import playlistElementSrc from "../../assets/playlist.svg";
+import shuffleElementSrc from "../../assets/shuffle.svg";
+import repeatElementSrc from "../../assets/repeat.svg";
+import eqElementSrc from "../../assets/EQ.svg";
+import favouritesElementSrc from "../../assets/favourites.svg";
 
 import { createElement } from "../../utils/elements";
 
 export const createControlElement = () => {
   const forwardElement = createElement("img", {
     src: forwardElementSrc,
-    alt: "play",
+    alt: "forward",
   });
   const playElement = createElement("img", {
     src: playElementSrc,
@@ -17,7 +22,32 @@ export const createControlElement = () => {
 
   const backwardElement = createElement("img", {
     src: backwardElementSrc,
-    alt: "play",
+    alt: "backward",
+  });
+
+  const playlistElement = createElement("img", {
+    src: playlistElementSrc,
+    alt: "playlist",
+  });
+
+  const shuffleElement = createElement("img", {
+    src: shuffleElementSrc,
+    alt: "shuffle",
+  });
+
+  const repeatElement = createElement("img", {
+    src: repeatElementSrc,
+    alt: "repeat",
+  });
+
+  const eqElement = createElement("img", {
+    src: eqElementSrc,
+    alt: "equalizer",
+  });
+
+  const favouritesElement = createElement("img", {
+    src: favouritesElementSrc,
+    alt: "favourites",
   });
 
   const controlElement = createElement("div", {
@@ -60,6 +90,7 @@ export const createControlElement = () => {
               alert("Playlist");
             },
             children: [
+              playlistElement,
               createElement("h5", {
                 innerText: "Playlist",
               }),
@@ -69,9 +100,10 @@ export const createControlElement = () => {
             className: "buttonRow_button__secondary",
             // children: [createElement[rewindElement],
             onclick: () => {
-              alert("Shufle!");
+              alert("Shuffle!");
             },
             children: [
+              shuffleElement,
               createElement("h5", {
                 innerText: "Shuffle",
               }),
@@ -84,6 +116,7 @@ export const createControlElement = () => {
               alert("Repeat!");
             },
             children: [
+              repeatElement,
               createElement("h5", {
                 innerText: "Repeat",
               }),
@@ -96,6 +129,7 @@ export const createControlElement = () => {
               alert("Equalize!");
             },
             children: [
+              eqElement,
               createElement("h5", {
                 innerText: "EQ",
               }),
@@ -108,6 +142,7 @@ export const createControlElement = () => {
               alert("Love!");
             },
             children: [
+              favouritesElement,
               createElement("h5", {
                 innerText: "Favourites",
               }),
