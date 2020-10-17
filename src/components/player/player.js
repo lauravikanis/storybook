9228437e2a174882f74d0d9c31020db954c335b3
lauravikanis <lauravikanis@yahoo.de>
2020-10-17 21:07:25 +0200

    add second row of player

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,64 +1,116 @@
-/* <div>
-  <div>
-    <button class="backward">
-      <img></img>
-    </button>
-    <button class="Play">
-      <img></img>
-    </button>
-    <button class="forward">
-      <img></img>
-    </button>
-  </div>
-  ;
-  <div>
-    <img></img>
-    <div class="playlist"></div>
-    <img></img>
-    <div class="shuffle"></div>
-    <img></img>
-    <div class="repeat"></div>
-    <img></img>
-    <div class="EQ"></div>
-    <img></img>
-    <div class="favourites"></div>
-  </div>
-</div>; */
-
+import "./player.css";
 import { createElement } from "../../utils/elements";
+
 export const createControlElement = (control) => {
   // hier fehlt noch was?
   const controlElement = createElement("div", {
-    className: "upperrow",
+    className: "Controls",
     children: [
-      createElement("button", {
-        className: "rewind",
-        innerText: "rewindbutton",
-        // children: [createElement[rewindElement],
-        onclick: () => {
-          alert("rewind me!");
-        },
+      createElement("div", {
+        className: "firstRow",
         children: [
-          createElement("h5", {
-            innerText: "Rewind",
+          createElement("button", {
+            className: "rewind",
+            // children: [createElement[rewindElement],
+            onclick: () => {
+              alert("rewind me!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "Play",
+              }),
+            ],
+          }),
+          createElement("button", {
+            className: "play",
+            // children: [playElement],
+            onclick: () => {
+              alert("play!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "Rewind",
+              }),
+            ],
+          }),
+          createElement("button", {
+            className: "forward",
+            // children: [forwardElement],
+            onclick: () => {
+              alert("forward me!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "forward",
+              }),
+            ],
           }),
         ],
       }),
-      createElement("button", {
-        className: "play",
-        innerText: "playbutton",
-        // children: [playElement],
-        onclick: () => {
-          alert("play!");
-        },
-      }),
-      createElement("button", {
-        className: "forward",
-        innerText: "forwardbutton",
-        // children: [forwardElement],
-        onclick: () => {
-          alert("forward me!");
-        },
+      createElement("div", {
+        className: "secondRow",
+        children: [
+          createElement("button", {
+            className: "subhead",
+            // children: [createElement[rewindElement],
+            onclick: () => {
+              alert("Playlist");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "Playlist",
+              }),
+            ],
+          }),
+          createElement("button", {
+            className: "subhead",
+            // children: [createElement[rewindElement],
+            onclick: () => {
+              alert("Shufle!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "Shuffle",
+              }),
+            ],
+          }),
+          createElement("button", {
+            className: "subhead",
+            // children: [createElement[rewindElement],
+            onclick: () => {
+              alert("Repeat!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "Repeat",
+              }),
+            ],
+          }),
+          createElement("button", {
+            className: "rewind",
+            // children: [createElement[rewindElement],
+            onclick: () => {
+              alert("Equalize!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "EQ",
+              }),
+            ],
+          }),
+          createElement("button", {
+            className: "rewind",
+            // children: [createElement[rewindElement],
+            onclick: () => {
+              alert("Love!");
+            },
+            children: [
+              createElement("h5", {
+                innerText: "Favourites",
+              }),
+            ],
+          }),
+        ],
       }),
     ],
   });
